431c8b1275dde91f41770da25d5637008e18358c
d-swe <djkim.swe@gmail.com>
2025-05-27 21:45:54 -0400

    email structure start

--- backend/email-starter/emails/email.tsx
+++ backend/email-starter/emails/email.tsx
@@ -5,14 +5,23 @@ export default function Email(props: { name: string, email: string, phone: strin
   return (
     <Tailwind>
       <Html>
-        <div className="">
+        <style>
+          {`
+          @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+          .custom-font {
+            font-family: 'Roboto', sans-serif;
+          }
+        `}
+        </style>
+        <div className="m-16 custom-font">
           <img
             src="https://raw.githubusercontent.com/d-swe/president-valet-ii/main/frontend/pvii-ui/src/assets/pvii-logo-ws.png"
             alt="President Valet II Logo"
             width="120"
           />
-          <h1>Hi, {props.name}</h1>
-          <p>Thank you for reach out to us! Someone from our team will get back to you in the next 24-48 hours. <br />Thank you!</p>
+          <h1>Hi, {props.name}!</h1>
+          <p className="text-xl">Thank you for reach out to us!<br /> Someone from our team will get back to you in the next 24-48 hours.</p>
+          <p className="text-xl">Thank you!</p>
         </div>
       </Html>
     </Tailwind >
